fc5724a932ebc2d4e861e599d7c8a0dc09196062
borsch <oleh.kurpiak@gmail.com>
2017-03-25 13:10:48 +0200

    init

---
+++ web-studio-kma/dao/BookEntityContext.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using web_studio.model;
+using System.Data.Entity;
+
+namespace web_studio.dao
+{
+    public class BookEntityContext : DbContext
+    {
+        public BookEntityContext()
+            : base("name=DbConnectionString")
+        {
+        }
+        public DbSet<Book> Books { get; set; }
+    }
+}
---
+++ web-studio-kma/web/Controllers/BookController.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Data.Entity;
+
+using web_studio.dao;
+using web_studio.model;
+using web_studio.web.Models;
+
+namespace web_studio.web.Controllers
+{
+    public class BookController : Controller
+    {
+        // GET: Book
+        public ActionResult Index()
+        {
+            try
+            {
+                //implementation of IDatabaseInitializer that will delete, recreate,
+                //and optionally re-seed the database with data only if the model has changed since the database was created.
+                //This implementation require you to use the type of the Database Context because it’s a generic class.
+                Database.SetInitializer(new DropCreateDatabaseIfModelChanges<BookEntityContext>());
+
+                return View();
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+        }
+
+        public ActionResult GetBookDetails()
+        {
+            var model = new List<BookModel>();
+            try
+            {
+                using (var context = new BookEntityContext())
+                {
+                    var value = context.Books.ToList();
+                    foreach (var book in value)
+                    {
+                        var bookModel = new BookModel();
+                        bookModel.BookName = book.BookName;
+                        bookModel.Author = book.Author;
+                        bookModel.Edition = book.Edition;
+                        bookModel.Publishing = book.Publishing;
+
+                        model.Add(bookModel);
+                    }
+                }
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+            return PartialView("_BookDetailView", model);
+        }
+
+        public ActionResult InsertBookDetail()
+        {
+            try
+            {
+                for (int counter = 0; counter < 5; counter++)
+                {
+                    var book = new Book()
+                    {
+                        BookName = "Book " + counter,
+                        Author = "Author " + counter,
+                        Edition = "Edition " + counter,
+                        Publishing = "Publishing " + counter
+                    };
+                    using (var context = new BookEntityContext())
+                    {
+                        context.Books.Add(book);
+                        context.SaveChanges();
+                    }
+                }
+
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+            return Json(true);
+        }
+    }
+}
