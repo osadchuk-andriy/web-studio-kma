004746d0c691b734fe14255856b9bb96886c5e74
borsch <oleh.kurpiak@gmail.com>
2017-04-11 15:13:36 +0300

    add users and projects

---
+++ web-studio-kma/web/model/Project.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace web.model
+{
+    [Table("projects")]
+    public class Project
+    {
+        [Key]
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public ProjectStatus Status { get; set; }
+        public virtual ICollection<UserToProject> Users { get; set; }
+    }
+}
---
+++ web-studio-kma/web/model/UserToProject.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace web.model
+{
+    [Table("users_to_project")]
+    public class UserToProject
+    {
+        [Key]
+        public int Id { get; set; }
+        public virtual User User { get; set; }
+        public virtual Project Project { get; set; }
+    }
+}
