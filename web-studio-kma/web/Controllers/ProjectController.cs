57d33d7356165c724913e23c4bca4603fc55b660
borsch <oleh.kurpiak@gmail.com>
2017-04-12 01:02:01 +0300

    add stuff to project page

---
+++ web-studio-kma/web/Controllers/ProjectController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+using web.model;
+using web_studio.dao;
+using web_studio.model;
+using web_studio.web.Models;
+
+namespace web.Controllers
+{
+    public class ProjectController : Controller
+    {
+        UsersEntityContext usersContext;
+
+        public ProjectController() //: this()
+        {
+            //101this(new UsersEntityContext());
+        }
+
+        public ProjectController(UsersEntityContext usersContext)
+        {
+            this.usersContext = usersContext;
+        }
+
+        // GET: Project
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
