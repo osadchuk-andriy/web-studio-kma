3c0e70a8d3a20fb83920e2fe7c50a3b5c70bc2f6
osadchuk-andriy <osadchukandriy7@gmail.com>
2017-04-12 19:40:02 +0300

    User page

--- web-studio-kma/web/Controllers/HomeController.cs
+++ web-studio-kma/web/Controllers/HomeController.cs
@@ -26,5 +26,6 @@ namespace web.Controllers
 
             return View();
         }
+ 
     }
 }
---
+++ web-studio-kma/web/Controllers/UserPageController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace web.Controllers
+{
+    public class UserPageController : Controller
+    {
+        //
+        // GET: /UserPage/
+        public ActionResult UserPage()
+        {
+            ViewBag.Message = "Your personal page.";
+
+            return View();
+        }
+	}
+}
